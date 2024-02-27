16ace9ca527ece0e15254da311bf0e3d1f85ff82
MariellaNik <mariela.nik02@gmail.com>
2024-02-27 21:23:59 +0200

    initial commit

---
+++ src/components/home/index.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+//import Loader from 'react-loaders'
+//import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+//import Logo from './Logo'
+import './index.scss'
+
+const Home =() => {
+    
+    return(
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1> Hello, <br /> I am 
+                <img src={LogoTitle} alt="developer"/>
+                Mariela
+                <br/>
+                web developer 
+                </h1>
+                <h2>Frontend Developer / Junior Student</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link> {/*link button*/}
+            </div>
+        </div>
+    )
+}
+
+export default Home
